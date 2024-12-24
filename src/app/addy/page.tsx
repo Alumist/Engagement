c3d3e8d3d4c3ae4761c1c04966313c391f1b45e0
Alumist <codealumist@gmail.com>
2024-12-23 16:52:16 -0800

    images displayed now...

--- src/app/addy/page.tsx
+++ src/app/addy/page.tsx
@@ -1,20 +1,34 @@
 import Image from 'next/image';
+import fs from 'fs';
+import path from 'path';
 
+const folderPath = path.join(process.cwd(), 'public/images/adelynsphotos');
+const listOfFiles = fs.readdirSync(folderPath);
 function AddyPage() {
-  return (
-    <div>
-      <h1 className='text-3xl flex items-center justify-center py-8'>
-        Shot by Addy
-      </h1>
-      <div className='flex flex-col justify-around'>
+  return listOfFiles.map((file) => {
+    return (
+      <div key={file} style={{ display: 'inline-block', margin: '4rem' }}>
         <Image
-          src='/images/adelynsphotos/097A7631.jpg'
+          src={`/images/adelynsphotos/${file}`}
           alt='addy'
-          width={300}
-          height={300}
+          width={200}
+          height={200}
         />
       </div>
-    </div>
-  );
+    );
+  });
+  // <div>
+  //   <h1 className='text-3xl flex items-center justify-center py-8'>
+  //     Shot by Addy
+  //   </h1>
+  //   <div className='flex flex-col justify-around'>
+  //     <Image
+  //       src='/images/adelynsphotos/097A7631.jpg'
+  //       alt='addy'
+  //       width={300}
+  //       height={300}
+  //     />
+  //   </div>
+  // </div>
 }
 export default AddyPage;
