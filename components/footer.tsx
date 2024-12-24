6600ef8bd42cdc1a7e8c1f8571f798f162698a07
Alumist <codealumist@gmail.com>
2024-12-23 23:05:09 -0800

    center...

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <div className='flex justify-center items-center text-5xl m-28 p-28 bg-[#fff2da] rounded-xl shadow-xl'>
+    <div className='flex justify-center items-center text-center mx-auto text-5xl m-28 p-28 bg-[#fff2da] rounded-xl shadow-xl'>
       <h1>Created By Nicholas Quiroz</h1>
     </div>
   );
